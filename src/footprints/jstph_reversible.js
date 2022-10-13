aa013bf9f61b3e3081c20795061c829ca3803b5f
Adam Fekete <adam@fekete.co.uk>
2022-10-13 12:59:22 +0200

    cleanup

--- src/footprints/jstph_reversible.js
+++ src/footprints/jstph_reversible.js
@@ -17,20 +17,20 @@ module.exports = {
         ${p.at /* parametric position */}
 
         ${'' /* footprint reference */}
-        (fp_text reference "${p.ref}" (at 0 0) (layer F.SilkS) ${p.ref_hide} (effects (font (size 1.27 1.27) (thickness 0.15))))
-        (fp_text value "" (at 0 0) (layer F.SilkS) hide (effects (font (size 1.27 1.27) (thickness 0.15))))
+        (fp_text reference "${p.ref}" (at 0 0) (layer F.Fab) ${p.ref_hide} (effects (font (size 1.27 1.27) (thickness 0.15))))
+        (fp_text value "" (at 0 0) (layer F.Fab) hide (effects (font (size 1.27 1.27) (thickness 0.15))))
 
-        (fp_line (start -2.25 0.25) (end -2.25 -1.35) (layer ${p.param.side}.SilkS) (width 0.15))
-        (fp_line (start -2.25 -1.35) (end -2.95 -1.35) (layer ${p.param.side}.SilkS) (width 0.15))
-        (fp_line (start -2.95 -1.35) (end -2.95 6.25) (layer ${p.param.side}.SilkS) (width 0.15))
-        (fp_line (start -2.95 6.25) (end 2.95 6.25) (layer ${p.param.side}.SilkS) (width 0.15))
-        (fp_line (start 2.95 6.25) (end 2.95 -1.35) (layer ${p.param.side}.SilkS) (width 0.15))
-        (fp_line (start 2.95 -1.35) (end 2.25 -1.35) (layer ${p.param.side}.SilkS) (width 0.15))
-        (fp_line (start 2.25 -1.35) (end 2.25 0.25) (layer ${p.param.side}.SilkS) (width 0.15))
-        (fp_line (start 2.25 0.25) (end -2.25 0.25) (layer ${p.param.side}.SilkS) (width 0.15))
+        (fp_line (start -2.25 0.25) (end -2.25 -1.35) (layer ${p.param.side}.Fab) (width 0.15))
+        (fp_line (start -2.25 -1.35) (end -2.95 -1.35) (layer ${p.param.side}.Fab) (width 0.15))
+        (fp_line (start -2.95 -1.35) (end -2.95 6.25) (layer ${p.param.side}.Fab) (width 0.15))
+        (fp_line (start -2.95 6.25) (end 2.95 6.25) (layer ${p.param.side}.Fab) (width 0.15))
+        (fp_line (start 2.95 6.25) (end 2.95 -1.35) (layer ${p.param.side}.Fab) (width 0.15))
+        (fp_line (start 2.95 -1.35) (end 2.25 -1.35) (layer ${p.param.side}.Fab) (width 0.15))
+        (fp_line (start 2.25 -1.35) (end 2.25 0.25) (layer ${p.param.side}.Fab) (width 0.15))
+        (fp_line (start 2.25 0.25) (end -2.25 0.25) (layer ${p.param.side}.Fab) (width 0.15))
 
-        (fp_line (start -1 1.5) (end -1 2.0) (layer ${p.param.side}.SilkS) (width 0.15))
-        (fp_line (start -1.25 1.75) (end -0.75 1.75) (layer ${p.param.side}.SilkS) (width 0.15))
+        (fp_line (start -1 1.5) (end -1 2.0) (layer ${p.param.side}.Fab) (width 0.15))
+        (fp_line (start -1.25 1.75) (end -0.75 1.75) (layer ${p.param.side}.Fab) (width 0.15))
 
         (pad "" thru_hole oval (at -1 0 ${p.rot}) (size 1.2 1.7) (drill 0.75) (layers *.Cu *.Mask) )
         (pad "" thru_hole oval (at 1 0 ${p.rot}) (size 1.2 1.7) (drill 0.75) (layers *.Cu *.Mask) )
